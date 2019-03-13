e6272fc9ef3e8def9cd6135d287a9d0b56ab020c
Chinedu Arinze <c.s.arinze@gmail.com>
2019-03-13 19:19:25 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,10 @@
 # code your #valid_move? method here
 
+def valid_move?(board, index)
+  if position_taken?(board, index)
+    puts "Please select a different move"
+  end
+end
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
@@ -11,9 +16,3 @@ def position_taken?(board, index)
     true
   end
 end
-
-  def valid_move?(board, index)
-    if position_taken?(board, index)
-      puts "Please select a different move"
-    end
-  end
